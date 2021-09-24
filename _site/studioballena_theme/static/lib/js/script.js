368c8dc6682bc016060253769adebc5e3971f42b
Jay <jaytrivedi840@gmail.com>
2021-09-24 12:50:09 +0530

    initial commit

--- _site/studioballena_theme/static/lib/js/script.js
+++ _site/studioballena_theme/static/lib/js/script.js
@@ -113,5 +113,6 @@ $('#play').on('click', function(e) {
 	e.preventDefault();
 	$("#player")[0].src += "?autoplay=1";
 	$('#player').show();
+	$('#video-cover').hide();
 	$('#play').hide();
-})
+});
